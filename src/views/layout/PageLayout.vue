84bb19cc1ab8d9e2c8f4cf46c03ea492fa771b01
xhh <jiatangxi@qq.com>
2025-08-20 10:24:56 +0800

    导入element plus并按需导入，完成了顶部的导航栏

--- src/views/layout/PageLayout.vue
+++ src/views/layout/PageLayout.vue
@@ -3,14 +3,35 @@ import '@/assets/base.css'
 </script>
 <template>
   <div class="nav">
-    <div class="w top">
-      <a href="#" data-hd="0">首页</a>
-      <a href="#">蛋糕</a>
-      <a href="#">西点</a>
-      <a href="#">常识</a>
-      <a href="#" data-hd="1">注册</a>
-      <a href="#" data-hd="2">登录</a>
-    </div>
+    <el-container>
+      <el-header>
+        <el-menu
+          class="el-menu-demo"
+          mode="horizontal"
+          default-active="/content/container"
+          router
+        >
+          <el-menu-item index="/content/container">首页</el-menu-item>
+          <el-menu-item index="/content/cake">蛋糕</el-menu-item>
+          <el-menu-item index="/content/west">西点</el-menu-item>
+          <el-menu-item index="/content/normal">常识</el-menu-item>
+          <el-menu-item index="/login">注册</el-menu-item>
+          <el-menu-item index="/register">登录</el-menu-item>
+        </el-menu>
+      </el-header>
+      <el-main><router-view></router-view></el-main>
+    </el-container>
   </div>
-  <router-view></router-view>
 </template>
+<style scoped>
+.el-menu {
+  width: 100%;
+  padding-left: 50%;
+}
+.el-header {
+  padding: 0;
+}
+.el-menu--horizontal > .el-menu-item {
+  margin: 0 20px;
+}
+</style>
