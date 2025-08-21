0359c255042de6a26b34c18a2c1bb13f60850efa
xhh <jiatangxi@qq.com>
2025-08-21 16:23:20 +0800

    注册登录基本交互完成，还差调用接口

--- src/views/login/PageLogin.vue
+++ src/views/login/PageLogin.vue
@@ -1,6 +1,40 @@
 <script setup>
 import { useRouter } from 'vue-router'
+import { ref } from 'vue'
+import { ElMessage } from 'element-plus'
 const router = useRouter()
+const username = ref('')
+const pwd = ref('')
+const nameerror = ref('')
+const pwderror = ref('')
+const namevalidate = () => {
+  const reg = /^[a-zA-Z0-9-_]{6,10}$/
+  if (!reg.test(username.value)) {
+    nameerror.value = '请输入合法的用户名'
+    return false
+  }
+  nameerror.value = ''
+  return true
+}
+const pwdvalidate = () => {
+  const reg = /^[a-zA-Z0-9]{6,16}$/
+  if (!reg.test(pwd.value)) {
+    pwderror.value = '请输入合法的密码'
+    return false
+  }
+  pwderror.value = ''
+  return true
+}
+const validate = () => {
+  const one = namevalidate()
+  const two = pwdvalidate()
+  if (one && two) {
+    ElMessage({
+      message: '注册成功',
+      type: 'success'
+    })
+  }
+}
 </script>
 <template>
   <div>
@@ -11,20 +45,31 @@ const router = useRouter()
       <div class="zhuce">
         <h5>免费注册</h5>
         <div class="name">
-          <input name="myname" type="text" placeholder="请输入用户名" />
-          <span></span>
+          <input
+            name="myname"
+            type="text"
+            placeholder="请输入用户名"
+            v-model="username"
+            @blur="namevalidate"
+          />
+          <span>{{ nameerror }}</span>
         </div>
         <div class="password">
-          <input name="password" type="password" placeholder="请输入密码" />
-          <span></span>
+          <input
+            name="password"
+            type="password"
+            placeholder="请输入密码"
+            v-model="pwd"
+            @blur="pwdvalidate"
+          />
+          <span>{{ pwderror }}</span>
         </div>
-        <button>立即注册</button>
+        <button @click="validate">立即注册</button>
         <div class="add">
           已有帐号，<a href="#" @click.prevent="router.push('/register')"
             >点击登录</a
           >
         </div>
-        <div class="suc">注册成功</div>
       </div>
     </div>
   </div>
--- src/views/login/PageRegister.vue
+++ src/views/login/PageRegister.vue
@@ -1,6 +1,40 @@
 <script setup>
 import { useRouter } from 'vue-router'
+import { ref } from 'vue'
+import { ElMessage } from 'element-plus'
 const router = useRouter()
+const username = ref('')
+const pwd = ref('')
+const nameerror = ref('')
+const pwderror = ref('')
+const namevalidate = () => {
+  const reg = /^[a-zA-Z0-9-_]{6,10}$/
+  if (!reg.test(username.value)) {
+    nameerror.value = '请输入合法的用户名'
+    return false
+  }
+  nameerror.value = ''
+  return true
+}
+const pwdvalidate = () => {
+  const reg = /^[a-zA-Z0-9]{6,16}$/
+  if (!reg.test(pwd.value)) {
+    pwderror.value = '请输入合法的密码'
+    return false
+  }
+  pwderror.value = ''
+  return true
+}
+const validate = () => {
+  const one = namevalidate()
+  const two = pwdvalidate()
+  if (one && two) {
+    ElMessage({
+      message: '登录成功',
+      type: 'success'
+    })
+  }
+}
 </script>
 <template>
   <div class="login tab" data-ad="2">
@@ -8,20 +42,31 @@ const router = useRouter()
     <div class="denglu">
       <h5>欢迎登录</h5>
       <div class="name">
-        <input name="myname" type="text" placeholder="请输入用户名" />
-        <span></span>
+        <input
+          name="myname"
+          type="text"
+          placeholder="请输入用户名"
+          v-model="username"
+          @blur="namevalidate"
+        />
+        <span>{{ nameerror }}</span>
       </div>
       <div class="password">
-        <input name="password" type="password" placeholder="请输入密码" />
-        <span></span>
+        <input
+          name="password"
+          type="password"
+          placeholder="请输入密码"
+          v-model="pwd"
+          @blur="pwdvalidate"
+        />
+        <span>{{ pwderror }}</span>
       </div>
-      <button>立即登录</button>
+      <button @click="validate">立即登录</button>
       <div class="add">
         没有帐号，<a href="#" @click.prevent="router.push('/login')"
           >点击注册</a
         >
       </div>
-      <div class="success">登陆成功</div>
     </div>
   </div>
 </template>
