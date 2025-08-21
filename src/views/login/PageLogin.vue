9297c1d83bd40bc806275fbb41fa644592671582
xhh <jiatangxi@qq.com>
2025-08-21 15:19:14 +0800

    登录注册静态页面渲染

--- src/views/login/PageLogin.vue
+++ src/views/login/PageLogin.vue
@@ -1,3 +1,31 @@
+<script setup>
+import { useRouter } from 'vue-router'
+const router = useRouter()
+</script>
 <template>
-  <div>我是注册</div>
+  <div>
+    <div class="register" data-ad="1">
+      <div class="back" @click="router.push('/content/container')">
+        返回首页
+      </div>
+      <div class="zhuce">
+        <h5>免费注册</h5>
+        <div class="name">
+          <input name="myname" type="text" placeholder="请输入用户名" />
+          <span></span>
+        </div>
+        <div class="password">
+          <input name="password" type="password" placeholder="请输入密码" />
+          <span></span>
+        </div>
+        <button>立即注册</button>
+        <div class="add">
+          已有帐号，<a href="#" @click.prevent="router.push('/register')"
+            >点击登录</a
+          >
+        </div>
+        <div class="suc">注册成功</div>
+      </div>
+    </div>
+  </div>
 </template>
--- src/views/login/PageRegister.vue
+++ src/views/login/PageRegister.vue
@@ -1,3 +1,27 @@
+<script setup>
+import { useRouter } from 'vue-router'
+const router = useRouter()
+</script>
 <template>
-  <div>我是登录</div>
+  <div class="login tab" data-ad="2">
+    <div class="back" @click="router.push('/content/container')">返回首页</div>
+    <div class="denglu">
+      <h5>欢迎登录</h5>
+      <div class="name">
+        <input name="myname" type="text" placeholder="请输入用户名" />
+        <span></span>
+      </div>
+      <div class="password">
+        <input name="password" type="password" placeholder="请输入密码" />
+        <span></span>
+      </div>
+      <button>立即登录</button>
+      <div class="add">
+        没有帐号，<a href="#" @click.prevent="router.push('/login')"
+          >点击注册</a
+        >
+      </div>
+      <div class="success">登陆成功</div>
+    </div>
+  </div>
 </template>
